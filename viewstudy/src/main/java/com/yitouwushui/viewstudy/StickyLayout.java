633039a99ceffb9334db1d81af00d8fd65583f51
dzx <280611948@qq.com>
2017-04-06 17:31:45 +0800

    test

--- viewstudy/src/main/java/com/yitouwushui/viewstudy/StickyLayout.java
+++ viewstudy/src/main/java/com/yitouwushui/viewstudy/StickyLayout.java
@@ -17,8 +17,9 @@ public class StickyLayout extends LinearLayout {
 
     private int mLastY = 0;
     // 分别记录上次滑动的坐标(onInterceptTouEvent)
-    private int mLastXInercept = 0;
-    private int mLastYInercept = 0;
+    private int mLastXIntercept = 0;
+    private int mLastYIntercept = 0;
+    private boolean mDisallowInterceptTouchEventOnHeader;
 
     public StickyLayout(Context context) {
         super(context);
@@ -30,6 +31,24 @@ public class StickyLayout extends LinearLayout {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int intercepted = 0;
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mLastXIntercept = x;
+                mLastYIntercept = y;
+                mLastX = x;
+                mLastY = y;
+                intercepted = 0;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - mLastXIntercept;
+                int deltaY = y - mLastYIntercept;
+                if (mDisallowInterceptTouchEventOnHeader)
+                break;
+        }
 
         return super.onInterceptTouchEvent(ev);
     }
