fc66df59cf83a3cf62a89f9e7e2d5c17e03f464b
dzx <280611948@qq.com>
2017-10-25 17:37:34 +0800

    最大区间

--- arithmetic/src/main/java/com/yitouwushui/arithmetic/KLeastNumbers.java
+++ arithmetic/src/main/java/com/yitouwushui/arithmetic/KLeastNumbers.java
@@ -1,5 +1,6 @@
 package com.yitouwushui.arithmetic;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /**
@@ -17,7 +18,42 @@ public class KLeastNumbers {
         test.getLeastNumbers(array, 2);
     }
 
-    private void getLeastNumbers(int[] array, int i) {
+    public void getLeastNumbers(int[] array, int k) {
+        if (array == null || k < 0 || k > array.length)
+            return;
+        int[] kArray = Arrays.copyOfRange(array, 0, k);
+        buildMaxHeap(kArray);
+        for (int i = k; i < array.length; i++) {
+            if (array[i] < kArray[0]) {
+                kArray[0] = array[i];
+                maxHeap(kArray, 0);
+            }
+        }
+        for (int i : kArray)
+            System.out.println(i);
+    }
 
+    private void maxHeap(int[] kArray, int i) {
+        int left = 2 * i;
+        int right = left + 1;
+        int largest = 0;
+        if (right < kArray.length && kArray[left] > kArray[i])
+            largest = left;
+        else
+            largest = i;
+        if (right < kArray.length && kArray[right] > kArray[largest])
+            largest = right;
+        if (largest != i) {
+            int temp = kArray[i];
+            kArray[i] = kArray[largest];
+            kArray[largest] = temp;
+            maxHeap(kArray, largest);
+        }
+    }
+
+    private void buildMaxHeap(int[] kArray) {
+        for (int i = kArray.length / 2; i >= 0; i--)
+            maxHeap(kArray, i);
     }
 }
+
